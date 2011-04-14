f2f2d746798906d8025b63a1a1b412c3f09ffc46
juhapekka.piiroinen <juhapekka.piiroinen@gmail.com>
2011-04-14 20:32:26 +0000

    added opencv compile scripts

---
+++ 3rdparty/opencv-build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+##################################
+# Author: Juhapekka Piiroinen
+# juhapekka.piiroinen@gmail.com
+# 2011
+# GPLv3
+##################################
+tar -xvjpf OpenCV-2.2.0.tar.bz2
+ln -s OpenCV-2.2.0 opencv
+mkdir opencv-build
+cd opencv-build
+cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_QT=ON -D WITH_TBB=YES -D WITH_V4L=0 -D TBB_INCLUDE_DIRS=/usr/include/tbb -D BUILD_SAMPLES=ON -D PYTHON_INCLUDE_DIRS=/usr/include/python2.7 ../opencv
+make -j`cat /proc/cpuinfo|grep processor|wc -l`
---
+++ 3rdparty/opencv-download.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+##################################
+# Author: Juhapekka Piiroinen
+# juhapekka.piiroinen@gmail.com
+# 2011
+# GPLv3
+##################################
+wget -c http://sourceforge.net/projects/opencvlibrary/files/opencv-unix/2.2/OpenCV-2.2.0.tar.bz2/download
+
