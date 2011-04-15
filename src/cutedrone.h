08489741271bbc10b3d2548eb507ad58ec8da796
juhapekka.piiroinen <juhapekka.piiroinen@gmail.com>
2011-04-15 18:56:59 +0000

    added FTRIM and basic manouvers. some of the code is from mardrone project.

--- src/cutedrone.h
+++ src/cutedrone.h
@@ -6,6 +6,8 @@
 #include <QHostAddress>
 #include <QThread>
 #include <QFile>
+#include <QTimer>
+#include <navdata_common.h>
 
 #define INT_32 unsigned long
 #define INT_16 unsigned int
@@ -37,18 +39,29 @@ class CuteDrone : public QThread {
 public:
     void initDrone();
     void run();
+    enum droneState {
+        notInitialized,
+        initialized,
+        ready,
+        flying
+    };
 
 protected slots:
     void telemetryReady();
     void videoReady();
     void commandSent(qint64);
     void sendCommand(QByteArray);
+    void sendNav(QByteArray);
 
+    void stateTimerElapsed();
     void takeOff();
     void land();
     void exit_bootstrap_mode();
     void receive_video();
     void receive_telemetry();
+    void setHorizontalLevel();
+    void setDroneControl(float pitch,float roll,float yaw,float vv);
+    void setDroneGain(float fgain,float bgain,float lgain,float rgain);
 
 protected:
     QHostAddress* address;
@@ -57,13 +70,29 @@ protected:
     QUdpSocket* commandAndControlCriticalSocket;
     QUdpSocket* telemetryAndTrackingSocket;
     QUdpSocket* telemetrySender;
+    QTimer* stateTimer;
     QFile log;
     QFile video;
+
     bool bInitialized;
     int counter;
     int m_iStartBit;
 
     static const char triggerByte[4];
+     int state;
+
+     float m_pitch;     // pitch in horizon
+     float m_roll;      // Roll in horizon
+     float m_yaw;       // low value to display
+     float m_vv;        // Verticl velocity
+     float m_altitude;  // altitude
+     float m_rotRefX;   // Rotation zero reference
+     float m_rotRefY;
+     float m_rotRefZ;
+     float m_fgain;
+     float m_bgain;
+     float m_rgain;
+     float m_lgain;
 };
 
 #endif // CUTEDRONE_H
