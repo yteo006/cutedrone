1010e8a6fdceeebd6966fdad9b7093d22feb6b31
juhapekka.piiroinen@gmail.com <juhapekka.piiroinen@gmail.com@c272c6c7-2de9-6a2d-a6fd-02fb958c9bc3>
2011-04-15 18:56:59 +0000

    added FTRIM and basic manouvers. some of the code is from mardrone project.

--- trunk/src/cutedrone.cpp
+++ trunk/src/cutedrone.cpp
@@ -14,8 +14,12 @@ void CuteDrone::initDrone()
            this->commandAndControlCriticalSocket = new QUdpSocket();
            this->telemetryAndTrackingSocket = new QUdpSocket();
            this->videoSocket = new QUdpSocket();
+           this->stateTimer = new QTimer();
+           this->stateTimer->setInterval(200);
+           connect(this->stateTimer,SIGNAL(timeout()),this,SLOT(stateTimerElapsed()));
+           this->stateTimer->start();
 
-       QHostAddress address = QHostAddress::Any;
+           QHostAddress address = QHostAddress::Any;
 
            if (!this->commandAndControlSocket->bind(address,5556)) {
                qDebug() << "FAIL: Could not bind commandAndControlSocket to 5556";
@@ -58,23 +62,45 @@ void CuteDrone::run() {
     connect(this->commandAndControlSocket,SIGNAL(readyRead()),this,SLOT(telemetryReady()));
     connect(this->videoSocket,SIGNAL(readyRead()),this,SLOT(videoReady()));
 
+    setDroneGain(1,1,2,1);
     telemetrySender = new QUdpSocket();
     connect(telemetrySender,SIGNAL(readyRead()),this,SLOT(telemetryReady()));
     connect(telemetrySender,SIGNAL(bytesWritten(qint64)),this,SLOT(commandSent(qint64)));
 
     m_iStartBit = (1<<18)|(1<<20)|(1<<22)|(1<<24)|(1<<28);
 
+    setHorizontalLevel();
+    QThread::msleep(500);
+
     takeOff();
     int wait = 0;
-    while (wait<5) {
+    while (wait<10) {
         // sendCommand(dgram);
+        QThread::msleep(500);
+        wait++;
+    }
+
+    state = flying;
+
+    // go right
+    setDroneControl(0,0,150,0);
+    wait = 0;
+    while (wait<5) {
+        QThread::msleep(500);
+        wait++;
+    }
+
+    // go left
+     setDroneControl(0,0,-150,0);
+    wait = 0;
+    while (wait<5) {
         QThread::msleep(500);
         wait++;
     }
 
     land();
     wait = 0;
-    while (wait<5) {
+    while (wait<20) {
         QThread::msleep(500);
         wait++;
         land();
@@ -82,6 +108,54 @@ void CuteDrone::run() {
     log.close();
     video.close();
  qDebug() << "<< run()";
+}
+
+void CuteDrone::setDroneControl(float pitch,float roll,float yaw,float vv)
+{
+    m_pitch=pitch/200.0*m_fgain;
+    m_roll=roll/200.0*m_rgain;
+    m_yaw=yaw/200.0;
+    m_vv=vv/200.0;
+    qDebug()  << QString("pitch=%1 roll=%2 yaw=%3 vv=%4\r").arg(m_pitch,3,'F',2).arg(m_roll,3,'F',2).arg(m_yaw,3,'F',2).arg(m_vv,3,'F',2);
+};
+
+void CuteDrone::setDroneGain(float fgain,float bgain,float lgain,float rgain)
+{
+    m_fgain=fgain;
+    m_bgain=bgain;
+    m_rgain=rgain;
+    m_lgain=lgain;
+};
+
+void CuteDrone::stateTimerElapsed()
+{
+    qDebug() << "thread Timer" << state;
+    switch(state) {
+        case notInitialized:
+            sendNav("AT");
+            break;
+        case initialized:
+            break;
+    case ready:
+            sendCommand(QString("AT*REF=%1,%2\r").arg(counter++).arg((1<<18) + (1<<20) + (1<<22) + (1<<24) +(1<<28)).toAscii());
+            break;
+    case flying: {
+            float_or_int_t _pitch,_roll,_yaw,_vv;
+            int r=(m_pitch!=0.0 || m_roll!=0.0 || m_yaw!=0.0)?1:0;
+
+            _pitch.f=m_pitch;
+            _roll.f=m_roll;
+            _yaw.f=m_yaw;
+            _vv.f=m_vv;
+//            qDebug()  << QString("AT*PCMD=%1,0,%2,%3,%4,%5\r").arg(seq).arg(_roll.f,3,'F',2).arg(_pitch.f,3,'F',2).arg(_vv.f,3,'F',2).arg(_yaw.f,3,'F',2);
+//            qDebug() << QString("AT*PCMD=%1,0,%2,%3,%4,%5\r").arg(seq).arg(_roll.i,8,16).arg(_pitch.i,8,16).arg(_vv.i,8,16).arg(_yaw.i,8,16);
+            sendCommand(QString("AT*COMWDG=%1\r").arg(counter++).toAscii());
+            sendCommand(QString("AT*PCMD=%1,%2,%3,%4,%5,%6\r").arg(counter++).arg(r).arg(_roll.i).arg(_pitch.i).arg(_vv.i).arg(_yaw.i).toAscii());
+
+
+            break;
+        }
+}
 }
 
 void CuteDrone::receive_telemetry() {
@@ -139,8 +213,20 @@ void CuteDrone::takeOff() {
      sendCommand(dgram);
 }
 
+void CuteDrone::setHorizontalLevel() {
+        QString cmd("AT*FTRIM=%0\r");
+        QByteArray dgram = cmd.arg(counter++).toLatin1();
+        sendCommand(dgram);
+        dgram = cmd.arg(counter++).toLatin1();
+        sendCommand(dgram);
+}
+
 void CuteDrone::sendCommand(QByteArray dgram) {
     commandAndControlSocket->writeDatagram(dgram.data(),dgram.size(),QHostAddress("192.168.1.1"),5556);
+}
+
+void CuteDrone::sendNav(QByteArray dgram) {
+    telemetryAndTrackingSocket->writeDatagram(dgram.data(),dgram.size(),QHostAddress("192.168.1.1"),5554);
 }
 
 void CuteDrone::commandSent(qint64 d) {
